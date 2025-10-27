6b8b6a1986ab4c1f49272a2eb8582f814df6d099
mhdlamine21 <mouhamedlniang@gmail.com>
2025-10-27 22:36:25 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//  Apparition au défilement
+
+const revealElements = document.querySelectorAll(".section, .timeline-item");
+
+const revealObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+revealElements.forEach(el => {
+  el.classList.add("hidden");
+  revealObserver.observe(el);
+});
